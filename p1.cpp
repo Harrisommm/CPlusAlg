0cbcfe194f90f56db59d42b55a17d77bb9a4780a
Harrison Kim <wootak95@gmail.com>
2023-03-19 00:12:04 +0900

    leet code practice

--- p1.cpp
+++ p1.cpp
@@ -1,7 +1,35 @@
 #include <iostream>
+#include <string>
+#include <cctype>
 
 int main()
 {
-    std::cout << "Hey";
-    return 0;
+    std::string s;
+    std::string reverse;
+    s = "Hey, You Here";
+    
+    for(int x=0; x<s.length(); x++)
+    {
+        s[x]=std::tolower(s[x]);
+    }
+    std::string result;
+    for(int y=0; y<s.length(); y++)
+    {
+        if(std::isalpha(s[y]))
+        {
+            result += s[y];
+        }
+    }
+    reverse = "";
+    int len = result.length();
+    std::cout << len;
+    reverse.resize(len);
+
+    
+    for (int i=0, j=len-1; i<len; i++, j--)
+    {
+        reverse[i] = result[j];
+    }
+    std::cout << reverse;
+
 };
