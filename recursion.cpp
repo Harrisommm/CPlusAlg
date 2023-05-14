97275ac6e7e074b40e3391a6b5b507f4e708d09d
HarrisonKim <wootak95@gmail.com>
2023-05-14 19:14:51 +0900

    natural number recursion practice modified:   note.txt deleted:    p1.cpp deleted:    p1.dSYM/Contents/Resources/DWARF/p1 renamed:    p1 -> recursion new file:   recursion.cpp renamed:    p1.dSYM/Contents/Info.plist -> recursion.dSYM/Contents/Info.plist new file:   recursion.dSYM/Contents/Resources/DWARF/recursion

---
+++ recursion.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+
+void treeRec(int n)
+{
+    if(n>0)
+    {
+        std::cout << n;
+        treeRec(n-1);
+        treeRec(n-1);
+    }
+}
+void indiRecB(int m);
+void indiRecA(int m)
+{
+    if(m>0)
+    {
+        std::cout << m;
+        indiRecB(m-1);
+    }
+}
+void indiRecB(int m)
+{
+    if (m>0)
+    {
+        std::cout << m;
+        indiRecA(m/2);
+    }
+}
+
+int nestedRec(int x)
+{
+    if(x>100)
+        return x-10;
+    return nestedRec(nestedRec(x+11));
+}
+
+int naturalSum(int y)
+{
+    if(y==0)
+    {
+        return y;
+    }
+    else
+        return naturalSum(y-1)+y;
+}
+
+int main()
+{
+    int r = naturalSum(5);
+    std::cout << r;
+    
+    return 0;
+
+};  
