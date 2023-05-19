955a0e24c45167b4e113aa88c7b2c949d28463a8
HarrisonKim <wootak95@gmail.com>
2023-05-19 16:33:50 +0900

    taylor series with memoization practice modified:   notes.txt modified:   recursion.cpp

--- recursion.cpp
+++ recursion.cpp
@@ -69,21 +69,37 @@
 //         return taylor(x,n-1);
 //     }
 // }
-int fib(int n)
-{
-    static int s;
-    if(n<=1)
-    {
-        return n;
-    }
-    else
-    {
-        return (fib(n-2)+fib(n-1));
-    }
-}
+// int F[10];
+// int fib(int n)
+// {
+    
+//     if(n<=1)
+//     {
+//         F[n] = n; //memoization
+//         return n;
+//     }
+//     else
+//     {
+//         if(F[n-2]==-1)
+//         {
+//             F[n-2] = fib(n-2);
+//         }
+//         if(F[n-1]==-1)
+//         {
+//             F[n-1] = fib(n-1);
+//         }
+//         F[n] = F[n-2] + F[n-1];
+//         return F[n];
+//     }
+// }
 
 int main()
 {
+    int i;
+    for(i=0; i<10; i++)
+    {
+        F[i] = -1;
+    }
     std::cout << fib(7);    
     return 0;
 
