fe704d989624161b0f7272d9b4bc4a71dd1bc37a
HarrisonKim <wootak95@gmail.com>
2023-06-17 23:39:04 +0900

    add basic matrix code practice 	modified:   codePrac/matrix.cpp 	modified:   notes.txt

--- codePrac/matrix.cpp
+++ codePrac/matrix.cpp
@@ -1,28 +1,74 @@
-struct Matrix
+#include <iostream>
+
+class Diagonal
 {
-    int A[10];
+private:
+    int *A;
     int n;
+
+public:
+    Diagonal()
+    {
+        n=2;
+        A=new int[2];
+    }
+
+    Diagonal(int n)
+    {
+        this->n = n;
+        A=new int[n];
+    }
+
+    ~Diagonal()
+    {
+        delete []A;
+    }
+
+    void Set(int i, int j, int x);
+    int Get(int i, int j);
+    void Display();
+
 };
 
-void Set(struct Matrix *m, int i, int j, int x)
+void Diagonal::Set(int i, int j, int x)
 {
     if(i==j)
-        m -> A[i-1]=x;
+        A[i-1]=x;
 }
 
-void Get(struct Matrix m, int i, int j)
+int Diagonal::Get(int i, int j)
 {
     if(i==j)
-        return m.A[i-1];
-    else
-        return 0;
+        return A[i-1];
+    return 0;
 }
 
-
+void Diagonal::Display()
+{
+    for(int i=0; i<n; i++)
+    {
+        for(int j=0; j<n; j++)
+        {
+            if(i==j)
+                std::cout<<A[i] << " ";
+            else    
+                std::cout<< "0 ";
+        }
+        std::cout<<std::endl;
+    }
+}
 
 int main()
 {
-    struct Matrix m;
+    Diagonal d(4);
+    d.Set(1,1,5);
+    d.Set(2,2,8);
+    d.Set(3,3,5);
+    d.Set(4,4,5);
+
+    d.Display();
+
+
 
 
     return 0;
